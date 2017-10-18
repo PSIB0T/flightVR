7e6e0ba13b186fd409998e7d76e0d906dab75659
Arvind Narayanan <arvindonarayanan@gmail.com>
2017-10-18 20:49:38 +0530

    Initial commit

---
+++ Assets/AutoWalk.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+  [RequireComponent(typeof(CharacterController))]
+public class AutoWalk : MonoBehaviour {
+    // How fast to move
+    public float speed = 3.0F;
+    // Should I move forward or not
+    public bool moveForward ;
+    // CharacterController script
+    private CharacterController controller;
+    // VR Head
+    private Transform vrHead;
+    
+    // Use this for initialization
+    void Start () {
+        // Find the CharacterController
+        controller = GetComponent<CharacterController>();
+       	
+        // Fnd the VR Head
+        vrHead = Camera.main.transform;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+        // In the Google VR button press
+        if (Input.GetButtonDown("Fire1")){
+            // Change the state of moveForward
+            moveForward = !moveForward;
+        }
+        
+        // Check to see if I should move
+        if (moveForward) {
+            // Find the forward direction
+            Vector3 forward = vrHead.TransformDirection(Vector3.forward);
+            // Tell CharacterController to move forward
+            controller.Move(forward * speed);
+        }
+    }
+}
---
+++ Assets/renderHeadPos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class renderHeadPos : MonoBehaviour {
+
+	// Use this for initialization
+	GameObject head;
+	public TextMesh text;
+	void Start () {
+		head = FindObjectOfType<GvrEditorEmulator>().gameObject;
+		text = GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = head.transform.rotation.x + "";
+	}
+}
